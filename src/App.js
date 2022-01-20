90a8e6bdfbdb66213ac324c435c53970d466b8d1
JoyKhweis <68160623+Jkhweis@users.noreply.github.com>
2022-01-20 16:14:37 -0600

    Create Global State

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import './App.css';
 import { Header } from './comps/Header';
 import { Balance } from './comps/Balance';
 import { IncomeExpenses } from './comps/IncomeExpenses';
+import { TransactionList } from './comps/TransactionList';
+import { AddTransaction } from './comps/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
